498bd6e3092a276ff4fe909890193340dd886751
ANireshkumar <niresh1606@gmail.com>
2025-01-09 15:54:38 +0530

    js expression, custom strylings

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
 {/*     <>Niresh react learing</>
     <LearnComponent/> */}
     <Header/>
+
+
     </>
   );
 }
--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,11 +1,21 @@
 
 
 export const Header = () => {
+
+  let customCSS = "code";
   return (
     <><div>
         <h1 className="bannerText">Hai! I am niresh</h1>
-        <p className="slogan">Learn react soon</p></div></>
-  )
+        <p className="slogan">Learn react soon</p>
+        
+        {/* javascript expression in jsx */}
+<p className={customCSS}>Changing the color with the cont default</p>
+<p className={customCSS}>45+55={45 + 55}</p> 
+<p className={customCSS}>This is to check the color blue violet</p>
+    <p style={{fontSize:"25PX", color:"darkblue"}}>This is in line styling</p>
+        </div></>
+
+)
 }
 
 export default Header;
